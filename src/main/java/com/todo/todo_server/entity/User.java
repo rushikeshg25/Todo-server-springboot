f34e866e9f4ad7b9d713c412a807b39c85d33f17
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-07-25 12:23:38 +0530

    User Entity and Psql config

---
+++ src/main/java/com/todo/todo_server/entity/User.java
@@ -0,0 +1,30 @@
+package com.todo.todo_server.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "email",nullable = false,unique = true)
+    private String email;
+
+}
