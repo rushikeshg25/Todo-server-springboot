5711749103533e01fda760d509a5277db510266c
rushikeshg25 <rushikeshvghotekar25@gmail.com>
2024-07-25 12:53:27 +0530

    Todo and User entity

--- src/main/java/com/todo/todo_server/repository/UserRepository.java
+++ src/main/java/com/todo/todo_server/repository/UserRepository.java
@@ -1,2 +1,7 @@
-package com.todo.todo_server.repository;public interface UserRepository {
+package com.todo.todo_server.repository;
+
+import com.todo.todo_server.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long> {
 }
